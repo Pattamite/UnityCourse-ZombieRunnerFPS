8d65593d797fc10dbc7ce669720302f94a3b7694
Pattnatt <jittapat007@gmail.com>
2018-08-22 21:22:31 +0300

    Add Player Spawn Points

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public GameObject spawnPoints;
+	public bool isRespawn = false;
+
+	// Use this for initialization
+	void Start() {
+		if (isRespawn) {
+			Respawn();
+		}
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+	}
+
+	void Respawn() {
+		int spawnPointsCount = spawnPoints.transform.childCount;
+		int pointToSpawn = Random.Range(0, spawnPointsCount);
+
+		this.transform.position = spawnPoints.transform.GetChild(pointToSpawn).transform.position;
+	}
+}
