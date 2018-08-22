ad52914171535c4723eaf5b9d05bb1a362ac3d1e
Pattnatt <jittapat007@gmail.com>
2018-08-22 22:00:21 +0300

    Add User Inout for zoom & call helicopter.

---
+++ Assets/Scripts/Eye.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eye : MonoBehaviour {
+
+	private Camera eyes;
+	private float defaultFOV;
+	public float zoomFOV;
+
+	// Use this for initialization
+	void Start () {
+		eyes = GetComponent<Camera>();
+		defaultFOV = eyes.fieldOfView;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButton("Zoom")){
+			eyes.fieldOfView = zoomFOV;
+		}
+		else {
+			eyes.fieldOfView = defaultFOV;
+		}
+	}
+}
